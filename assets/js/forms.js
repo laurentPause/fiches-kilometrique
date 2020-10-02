3abc90bdbe01effb2616ee558bff8c3b3b40f385
laurentPause <48903538+laurentPause@users.noreply.github.com>
2020-10-02 08:58:22 +0400

    requete add generaciter tester

--- assets/js/forms.js
+++ assets/js/forms.js
@@ -1,24 +1,7 @@
 const axios = require('axios').default;
 
 async function add (formName, fieldsString, model) {
-    // event.preventDefault();    
     try {
-        // const {
-        //     nom,
-        //     prenom,
-        //     fonction
-        // } = document.individus;
-
-        // const options = {
-        //     model : 'Individus',
-        //     data : {
-        //         nom : nom.value,
-        //         prenom : prenom.value,
-        //         fonction : fonction.value
-        //     }
-        // }
-        // console.log('add individu:', options);
-        // const individu = await axios.post('/api/add',options)
         const form = document[formName];
         const fields = fieldsString.split(',');
         const options = {
