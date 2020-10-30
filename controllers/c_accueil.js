3a5922fe4b6a183e4abf98219e21fc0cd376ed86
laurentPause <48903538+laurentPause@users.noreply.github.com>
2020-10-30 11:58:53 +0400

    init dashboard admin

--- controllers/c_accueil.js
+++ controllers/c_accueil.js
@@ -4,11 +4,16 @@ const Roles = require("../models/Roles");
 exports.dashboard = async (req, res, next) => {
     
     const user = req.session.user;
+    let view = 'pages'
+    if(user.Role == 'Admin'){
+        view += '/admins';
+    }
+    console.log(user);
     const options ={
         layout: 'layout/dashboard',
         title: 'Tableau de bord',
         user: user
     }
 
-    res.render('pages',options)
+    res.render(view,options)
 }
